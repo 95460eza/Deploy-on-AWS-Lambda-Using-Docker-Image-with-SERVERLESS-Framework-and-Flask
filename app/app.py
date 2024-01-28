28af1478a7156d20e89236973226674027d81e31
95460eza <alfredkoffisokpa@gmail.com>
2024-01-28 18:51:39 -0500

    Used AWS Lambda-generated event to fix wsgi_env in handler function

--- app/app.py
+++ app/app.py
@@ -38,7 +38,21 @@ def lambda_handler(event, context):
             # 'wsgi.version': event['wsgi.version'],
             'wsgi.version': (1, 0),  # Use a default version
             'wsgi.url_scheme': event['headers']['CloudFront-Forwarded-Proto'],
-            'REQUEST_METHOD': event['httpMethod']
+            'REQUEST_METHOD': event['httpMethod'],
+            #'wsgi.input': event['body'],
+
+            'SCRIPT_NAME': event['requestContext']['path'],
+            'PATH_INFO': event['path'],
+            'QUERY_STRING': event['queryStringParameters'],
+            'SERVER_NAME': event['headers']['Host'],
+            'SERVER_PORT': event['headers']['X-Forwarded-Port'],
+            'SERVER_PROTOCOL': event['headers']['X-Amzn-Trace-Id'],
+            'HTTP_ACCEPT': event['headers']['Accept'],
+            'HTTP_ACCEPT_ENCODING': event['headers']['Accept-Encoding'],
+            'HTTP_USER_AGENT': event['headers']['User-Agent'],
+            'CONTENT_TYPE': event['headers'].get('Content-Type', ''),
+            'CONTENT_LENGTH': event['headers'].get('Content-Length', '')
+
             # Add more relevant headers as needed
         }
 
